53b0e394ff03108624b206e9fdfdd6df0dfeece8
ss <savchev@rtfglobal.com>
2019-10-30 00:39:45 -0400

    Added UI controls

--- app/src/main/java/com/snibble/demoappkotlin/MainActivity.kt
+++ app/src/main/java/com/snibble/demoappkotlin/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.snibble.demoappkotlin
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.net.URL
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Handler
+import android.os.Looper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler: Handler = Handler(Looper.getMainLooper())
 
     class APIColor {
         val red: Float = 0.0f;
@@ -26,13 +31,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callAPIColor()
     }
 
     fun callAPIColor() {
-        val entireJson = URL("http://51.79.20.91/0ss/color.php").readText()
+        val thread = Thread {
+            try {
+                val entireJson = URL("http://51.79.20.91/0ss/color.php").readText()
 
-        val mapper = jacksonObjectMapper()
+                val mapper = jacksonObjectMapper()
 
-        val color = mapper.readValue<APIColor>(entireJson)
+                val color = mapper.readValue<APIColor>(entireJson)
+                updateAPIColor(color)
+            } catch (e: Exception) {
+                e.printStackTrace()
+
+                updateBackgroundColor(Color.RED);
+            }
+        }
+        thread.start()
+    }
+
+    fun updateBackgroundColor(color: Int) {
+        handler.post {
+            this.window.decorView.setBackgroundColor(color)
+        }
+    }
+
+    fun updateAPIColor(color: APIColor) {
+        handler.post {
+            //this.window.decorView.setBackgroundColor(color)
+        }
     }
 }
